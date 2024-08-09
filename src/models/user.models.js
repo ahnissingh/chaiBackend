bb46df807a7304433584bf9c31a50183d80b4b38
Ahnis <ahnisaneja@gmail.com>
2024-08-09 20:22:17 +0530

    Fixed typos

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,7 +1,6 @@
 import mongoose, { Schema, model } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-
 const userSchema = new Schema({
     username: {
       type: String,

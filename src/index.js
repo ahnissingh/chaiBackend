ab59d3c3c253a082e3109e6dc892ed41d51976c4
Ahnis <ahnisaneja@gmail.com>
2024-08-09 17:21:03 +0530

    Added mongoDb Config

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// require('dotenv')
+//   .config({path:'./env'})
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+dotenv.config({
+  path: "./env",
+});
+
+/*import express from "express";
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error",(error)=>{
+      console.log("errr");
+      throw  error
+    })
+    app.listen(process.env.PORT,()=>{
+      console.log(`Listening on port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+})();
+ */
+
